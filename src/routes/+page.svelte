7d819de7d327e8cc68577ae314ec6942ddf35677
jh_byun <jh_byun@opstech.kr>
2024-04-04 22:22:38 +0900

    feat: add category component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script>
   import Category from "./Category.svelte";
-  import welcome from "$lib/images/svelte-welcome.webp";
-  import welcome_fallback from "$lib/images/svelte-welcome.png";
-  /**
-   * TODO: 이 곳에 등록한 카테고리 정보들이 뜰 것임
-   * TODO: 근데 좀 이쁘게 뜨는 것을 원함.
-   */
 </script>
 
 <svelte:head>
@@ -13,54 +7,8 @@
   <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-  <h1>
-    <span class="welcome">
-      <picture>
-        <source srcset={welcome} type="image/webp" />
-        <img src={welcome_fallback} alt="Welcome" />
-      </picture>
-    </span>
-
-    to your new<br />SvelteKit app
-  </h1>
-
-  <h2>
-    try editing <strong>src/routes/+page.svelte</strong>
-  </h2>
-
+<section class="m-4">
   <Category />
 </section>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 0.6;
-  }
-
-  h1 {
-    width: 100%;
-  }
-
-  .welcome {
-    display: block;
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
-</style>
+<style></style>
