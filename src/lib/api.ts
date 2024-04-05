25c7480ae9b56785b3f7e6e508a064ddaae3a8ab
jh_byun <jh_byun@opstech.kr>
2024-04-05 22:00:08 +0900

    feat: 카테고리 전체 조회 api 호출 추가

---
+++ src/lib/api.ts
@@ -0,0 +1,17 @@
+// src/lib/api.ts
+const API_URL = "http://localhost:3000/api";
+
+export async function fetchCategories() {
+  try {
+    const response = await fetch(`${API_URL}/v1/categories`);
+    console.log(`${API_URL}/v1/categories: ${API_URL}/v1/categories`);
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+    throw error;
+  }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,15 @@
   <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section class="m-4">
+<!-- 
+  TODO: 마이크로소프트 앱 나오는 것 처럼 디자인 할 것임
+  @link https://www.samsungsvc.co.kr/proxy?isAttach=true&fileInfo=KTllLV9rTGNCS0JsMEJDLksvfERfK3kjQC5lNC9dQ3kyL2VCWlw/PSEqbGV8ayZtIV4zTDRNLTlra3xjeUF5JUB4emUyMm4vRC9ENy5dYD9LNV1ubkB7KClhZS1uQltea0tKbE5rejUpTl9MWDNDNil8XS0rQiQjKyYxRC9LLCEyfDE0akRbeSs6bEw6fi0jQV5jWCZLNF1BTkw__C__&fileName=8-1.jpg&fromNamo=true&faqFlag=true
+  - 이런 느낌!
+
+  FIXME: 카테고리가 '비율'로 나오게끔 바꿔야 함
+  - 현재는 비율로 나오지 않아 반응형에 굉장히 취약하다.
+ -->
+<section class="m-4 grid grid-cols-4 px-20 py-8">
   <Category />
 </section>
 
