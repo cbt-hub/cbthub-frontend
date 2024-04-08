24c139f91dffd9c63a77cce19d958e7e1cbc3c1b
jh_byun <jh_byun@opstech.kr>
2024-04-09 00:43:28 +0900

    feat: solve question api 연동

--- src/lib/apis/question/question.api.ts
+++ src/lib/apis/question/question.api.ts
@@ -1,5 +1,8 @@
 import { API_URL } from "../api";
 
+/**
+ * @description Round ID에 해당하는 단일 문제를 가져오는 API
+ */
 export async function fetchQuestion(id: string): Promise<ApiResponse> {
   const token = localStorage.getItem("access_token"); // 'auth_token'을 실제 토큰이 저장된 키로 변경하세요.
 
@@ -21,6 +24,39 @@ export async function fetchQuestion(id: string): Promise<ApiResponse> {
       },
     });
   }
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  return response.json();
+}
+
+/**
+ * @description 정답 제출 API
+ */
+export async function solveQuestion(
+  questionId: string,
+  questionDetailsId: string
+): Promise<any> {
+  console.log("questionId", questionId);
+  console.log("questionDetailsId", questionDetailsId);
+
+  const token = localStorage.getItem("access_token");
+  if (!token) {
+    throw new Error("access_token is not found");
+  }
+  const response = await fetch(`${API_URL}/v1/questions/${questionId}/solve`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      questionDetailsId: questionDetailsId,
+    }),
+  });
 
   if (!response.ok) {
     throw new Error("Network response was not ok");
--- src/routes/round/[id]/+page.svelte
+++ src/routes/round/[id]/+page.svelte
@@ -1,20 +1,42 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { page } from "$app/stores";
-  import type { Page } from "@sveltejs/kit";
-  import { fetchQuestion } from "$lib/apis/question/question.api";
+  import {
+    fetchQuestion,
+    solveQuestion,
+  } from "$lib/apis/question/question.api";
 
-  // `details`가 항상 배열이 되도록 초기값 설정
   let question: any = {
-    details: [], // 여기서 `details`를 빈 배열로 초기화
+    details: [],
+    explains: [],
   };
+  let roundId: string = "";
+  let selectedDetailId: string | null = null; // 선택된 detail의 ID를 저장하는 상태
 
   onMount(async () => {
-    const roundId = $page.params.id; // 현재 페이지의 roundId 가져오기
+    roundId = $page.params.id;
     if (roundId) {
-      question = await fetchQuestion(roundId); // API 호출 결과를 question에 할당
+      question = await fetchQuestion(roundId);
     }
   });
+
+  // detail 선택 이벤트 핸들러
+  function selectDetail(detailId: string): void {
+    selectedDetailId = detailId; // 선택된 detail의 ID를 저장
+  }
+
+  // 사용자가 정답 확인 버튼을 클릭했을 때, 정답을 확인합니다.
+  async function checkAnswer(): Promise<void> {
+    console.log(`정답 확인 : ${question.id}, ${selectedDetailId}`);
+    
+    if (!selectedDetailId) return;
+    try {
+      const result = await solveQuestion(question.id, selectedDetailId);
+      console.log(result);
+    } catch (error) {
+      console.error("정답 확인 중 오류가 발생했습니다:", error);
+    }
+  }
 </script>
 
 <svelte:head>
@@ -22,21 +44,41 @@
   <meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="flex justify-center flex-col max-w-2xl">
-  <div>
-    <div class="flex items-center">
-      <p class="text-3xl">{question.id}</p>
+<div class="flex justify-center flex-col max-w-2xl mt-16">
+  <div class="question-container mb-8">
+    <div class="flex items-center mb-4">
+      <p class="text-2xl text-blue-400 mr-2">{question.id}.</p>
       <h1 class="text-2xl">{question.title}</h1>
     </div>
 
-    <p>{question.content}</p>
+    <p class="border-2 p-4 mb-4">{question.content}</p>
     <ul>
       {#each question.details as detail, index (detail.id)}
-        <li class="flex">
-          <p>{index + 1}</p>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+        <li
+          class="flex items-center pr-1 py-1"
+          on:click={() => selectDetail(detail.id)}
+          class:text-blue-400={selectedDetailId === detail.id}
+        >
+          <p
+            class="rounded-full text-sm border-2 w-6 h-6 text-center mr-2"
+            style={selectedDetailId === detail.id
+              ? "--tw-bg-opacity: 1;border-color: rgb(96 165 250 / var(--tw-bg-opacity));"
+              : "gray-400"}
+          >
+            {index + 1}
+          </p>
           <p>{detail.choice}</p>
         </li>
       {/each}
     </ul>
+  </div>
+  <div class="explain-container flex">
+    <button
+      on:click={checkAnswer}
+      class="text-white text-lg rounded-lg bg-blue-400 px-3 py-2"
+      >정답 확인</button
+    >
   </div>
 </div>
