9d74856198386e55c8736ca7f66e2947499cbfbc
jh_byun <jh_byun@opstech.kr>
2024-04-12 00:58:46 +0900

    feat: 이전 / 다음 문제 버튼 로직

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,47 +1,68 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
-  import { login } from "$lib/api";
+  import { login } from "$lib/apis/api";
 
-	let email = '';
-	let password = '';
+  let email = "";
+  let password = "";
 
-	async function handleLogin(e: SubmitEvent) {
-		e.preventDefault();
-		try {
-			const data = await login(email, password);
-			console.log('로그인 성공:', data);
-			localStorage.setItem('access_token', data.access_token); // 발급받은 JWT를 로컬 스토리지에 저장
-			location.href = '/';
-		} catch (error) {
-			console.error((error as Error).message);
-			// TODO: 실패 로직 처리
-		}
-	}
+  async function handleLogin(e: SubmitEvent) {
+    e.preventDefault();
+    try {
+      const data = await login(email, password);
+      console.log("로그인 성공:", data);
+      localStorage.setItem("access_token", data.access_token); // 발급받은 JWT를 로컬 스토리지에 저장
+      location.href = "/";
+    } catch (error) {
+      console.error((error as Error).message);
+      // TODO: 실패 로직 처리
+    }
+  }
 
-	/**
-	 * TODO:
-	 * 1. 비밀번호 찾기 기능
-	 * 2. 약관
-	 * 3. 소셜 로그인 연동
-	*/
+  /**
+   * TODO:
+   * 1. 비밀번호 찾기 기능
+   * 2. 약관
+   * 3. 소셜 로그인 연동
+   */
 </script>
 
 <svelte:head>
-	<title>cbthub - 로그인</title>
-	<meta name="description" content="About this app" />
+  <title>cbthub - 로그인</title>
+  <meta name="description" content="About this app" />
 </svelte:head>
 
 <div class="text-column flex justify-center flex-col items-center h-full mt-40">
-	<h1 class="text-2xl mb-5 w-72">다시 오신 것을 환영합니다!</h1>
-	<div class="flex justify-center">
-		<form on:submit={handleLogin} class="flex justify-center flex-col w-72 items-center">
-			<input type="email" bind:value={email} placeholder="이메일 주소" class="input-box rounded-lg p-3 mb-1 border-2 w-72">
-			<input type="password" bind:value={password} placeholder="비밀번호" class="input-box rounded-lg p-3 mb-1 border-2 w-72">
-			<p class="text-left w-full my-4 text-sm text-sky-400">비밀번호를 잊어버리셨나요?</p>
-			<button type="submit" class="mb-4 bg-sky-300 text-gray-50 rounded-lg p-3 border-2 w-72">로그인</button>
-		</form>
-	</div>
-	<p class="text-sm">계정이 없으신가요?<a class="pl-2 text-sky-400" href="/join">가입하기</a></p>
+  <h1 class="text-2xl mb-5 w-72">다시 오신 것을 환영합니다!</h1>
+  <div class="flex justify-center">
+    <form
+      on:submit={handleLogin}
+      class="flex justify-center flex-col w-72 items-center"
+    >
+      <input
+        type="email"
+        bind:value={email}
+        placeholder="이메일 주소"
+        class="input-box rounded-lg p-3 mb-1 border-2 w-72"
+      />
+      <input
+        type="password"
+        bind:value={password}
+        placeholder="비밀번호"
+        class="input-box rounded-lg p-3 mb-1 border-2 w-72"
+      />
+      <p class="text-left w-full my-4 text-sm text-sky-400">
+        비밀번호를 잊어버리셨나요?
+      </p>
+      <button
+        type="submit"
+        class="mb-4 bg-sky-300 text-gray-50 rounded-lg p-3 border-2 w-72"
+        >로그인</button
+      >
+    </form>
+  </div>
+  <p class="text-sm">
+    계정이 없으신가요?<a class="pl-2 text-sky-400" href="/join">가입하기</a>
+  </p>
 </div>
 
 <style>
--- src/routes/round/[id]/+page.svelte
+++ src/routes/round/[id]/+page.svelte
@@ -6,6 +6,7 @@
     solveQuestion,
   } from "$lib/apis/question/question.api";
   import { writable, type Writable } from "svelte/store";
+  import { goto } from "$app/navigation";
 
   interface Detail {
     id: string;
@@ -21,6 +22,7 @@
   let question: any = {
     details: [],
     explains: [],
+    questionMeta: {},
   };
   let roundId: string = "";
   let selectedDetailId: string | null = null; // 선택된 detail의 ID를 저장하는 상태
@@ -32,6 +34,8 @@
     if (roundId) {
       question = await fetchQuestion(roundId, questionId ?? ""); // 쿼리 파라미터와 함께 fetchQuestion을 호출합니다.
     }
+
+    console.log("question:", question);
   });
 
   /** detail 선택 이벤트 핸들러 */
@@ -39,17 +43,32 @@
     selectedDetailId = detailId; // 선택된 detail의 ID를 저장
   }
 
+  /** 이전 문제로 이동 */
+  function prev(): void {
+    location.href = `/round/${roundId}?questionId=${question.questionMeta.prev}`;
+  }
+
+  /** 다음 문제로 이동 */
+  function next(): void {
+    location.href = `/round/${roundId}?questionId=${question.questionMeta.next}`;
+  }
+
   /**
    * TODOS:
    * TODO: 정답인지 오답인지 표시하는 로직이 필요합니다.
    * TODO: 정답 확인 버튼을 누르면, 정답 확인 글씨가 정답 접기 글씨로 바뀌고 해설이 보이지 않게 바꿉니다.
-   * TODO: 다음문제, 이전문제 버튼을 누르면, 다음 문제로 넘어가거나 이전 문제로 넘어갑니다.
+   * FIXED: 다음문제, 이전문제 버튼을 누르면, 다음 문제로 넘어가거나 이전 문제로 넘어갑니다. ✔️
    * TODO: 해설 및 보기를 보여주는 곳에 마크다운 형식의 스타일이 필요합니다. * 줄바꿈이 필요합니다.
-   * - 가장 첫번째 문제와 마지막 문제에서는 이전 문제, 다음 문제 버튼이 비활성화 되는 로직이 필요합니다.
-   * 
+   *    - 가장 첫번째 문제와 마지막 문제에서는 이전 문제, 다음 문제 버튼이 비활성화 되는 로직이 필요합니다.
+   * TODO: 이전 문제, 다음 문제의 버튼이 고정되어 있어야 합니다.
+   *    - 아래에 있을 지, 그냥 맨 위로 둘지 고민이다.
+   *
    * 추후 만들고 싶은 기능!
    * TODO: OMR 기능을 추가하여, 사용자가 문제를 빠르게 탐색 가능하게 합니다.
    * TODO: 사용자가 문제를 풀 때마다 통계가 자동으로 기록되어 문제별 정답률을 제공합니다.
+   *
+   * 최적화!
+   * TODO: 문제를 불러올 때, 미리 전 후 다섯 개의 문제를 불러와 사용자가 빠르게 문제를 탐색할 수 있게 합니다.
    */
 
   /** 사용자가 정답 확인을 클릭했을 때 발생하는 로직 */
@@ -62,8 +81,8 @@
       const result = await solveQuestion(question.id, selectedDetailId);
       correctDetails.set(
         question.details
-          .map((detail, index) => ({ detail, index }))
-          .filter(({ detail }) => detail.isCorrect)
+          .map((detail: Detail, index: number) => ({ detail, index }))
+          .filter(({ detail }: { detail: Detail }) => detail.isCorrect)
       );
     } catch (error) {
       console.error("정답 확인 중 오류가 발생했습니다:", error);
@@ -76,7 +95,7 @@
   <meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="flex justify-center flex-col max-w-2xl mt-16">
+<div class="flex justify-center flex-col w-3/5 mt-16">
   <div class="question-container mb-8">
     <div class="flex items-center mb-4">
       <p class="text-2xl text-blue-400 mr-2">{question.id}.</p>
@@ -115,38 +134,64 @@
       >
     </div>
     <div class="flex">
-      <button
-        on:click={checkAnswer}
-        class="text-white text-lg rounded-full bg-blue-400 px-3 py-2 mr-3 shadow-md"
-        ><i class="fa-solid fa-arrow-left"></i></button
-      >
-      <button
-        on:click={checkAnswer}
-        class="text-white text-lg rounded-full bg-blue-400 px-3 py-2 shadow-md flex items-center"
-        ><p class="mr-2">다음 문제</p>
-        <i class="fa-solid fa-arrow-right"></i>
-      </button>
+      {#if question.questionMeta.prev === null}
+        <button
+          on:click={prev}
+          class="text-white text-lg rounded-full bg-gray-400 px-3 py-2 mr-3 shadow-md"
+          disabled><i class="fa-solid fa-arrow-left"></i></button
+        >
+      {:else}
+        <button
+          on:click={prev}
+          class="text-white text-lg rounded-full bg-blue-400 px-3 py-2 mr-3 shadow-md"
+          ><i class="fa-solid fa-arrow-left"></i></button
+        >
+      {/if}
+
+      {#if question.questionMeta.next === null}
+        <button
+          on:click={next}
+          class="text-white text-lg rounded-full px-3 py-2 shadow-md flex items-center bg-gray-400"
+          disabled
+          ><p class="mr-2">다음 문제</p>
+          <i class="fa-solid fa-arrow-right"></i>
+        </button>
+      {:else}
+        <button
+          on:click={next}
+          class="text-white text-lg rounded-full px-3 py-2 shadow-md flex items-center bg-blue-400"
+          ><p class="mr-2">다음 문제</p>
+          <i class="fa-solid fa-arrow-right"></i>
+        </button>
+      {/if}
     </div>
   </div>
   <div class="answers py-4">
     {#each $correctDetails as { detail, index }}
       <div class="w-full border-t-2 mb-4"></div>
       <div class="correct-detail flex flex-col mb-4">
-        <div class="flex items-center justify-center bg-green-200 text-gray-600 rounded-sm shadow-md mb-2 w-fit py-1 px-2">
+        <div
+          class="flex items-center justify-center bg-green-200 text-gray-600 rounded-sm shadow-md mb-2 w-fit py-1 px-2"
+        >
           <p class="text-xl mr-2">정답:</p>
-          <p class="rounded-full text-sm border-2 w-6 h-6 text-center border-gray-600">
+          <p
+            class="rounded-full text-sm border-2 w-6 h-6 text-center border-gray-600"
+          >
             {index + 1}
           </p>
         </div>
         <p>{detail.choice}</p>
       </div>
       {#each question.explains as explain}
-      <div class="correct-detail flex flex-col mb-4">
-        <div class="flex items-center justify-center bg-green-200 text-gray-600 rounded-sm shadow-md mb-2 w-fit py-1 px-2">
-          <p class="text-xl mr-2">해설:</p><p class="text-xl">{explain.type}</p>
-        </div>
+        <div class="correct-detail flex flex-col mb-4">
+          <div
+            class="flex items-center justify-center bg-green-200 text-gray-600 rounded-sm shadow-md mb-2 w-fit py-1 px-2"
+          >
+            <p class="text-xl mr-2">해설:</p>
+            <p class="text-xl">{explain.type}</p>
+          </div>
           <p>{explain.explain}</p>
-      </div>
+        </div>
       {/each}
     {/each}
   </div>
